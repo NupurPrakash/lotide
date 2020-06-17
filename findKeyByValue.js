60cc020ac7d40e1648aaaa6be4d74ca110ac8eb3
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-17 20:09:43 +0000

    Accessing key and values of object

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKeyByValue = function(obj,str) {
+  for(let key in obj) {
+    //console.log("Keys:",key);
+    //console.log("Value:",obj[key]);
+    if(obj[key] === str) {
+      return key;
+    }
+   
+
+    }         
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
