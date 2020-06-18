1faaa95b6243d9437ac5ec3fc8aea8fefb83ae0f
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-18 17:29:42 +0000

    after updating eqarrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,13 @@
 const eqArrays = function(array1,array2) {
-  let match = true;
+  if(array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      match = false;
+      return false;
     }
   }
-  return match;
+  return true;
 
 };
 const assertArraysEqual = function(val1,val2) {
@@ -15,4 +17,4 @@ const assertArraysEqual = function(val1,val2) {
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); 
