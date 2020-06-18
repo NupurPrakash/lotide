d0e5872e57d00a64574e97ab7bb18970321d01ac
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-18 17:31:25 +0000

    updated eqarrays

--- without.js
+++ without.js
@@ -1,12 +1,14 @@
 
 const eqArrays = function(array1,array2) {
-  let match = true;
+  if(array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      match = false;
+      return false;
     }
   }
-  return match;
+  return true;
 
 };
 const assertArraysEqual = function(val1,val2) {
