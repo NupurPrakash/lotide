c151ecb48a5bf8de08dab0e46c6ec6903154c8cf
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-21 20:34:40 +0000

    Used mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,4 @@
+/*
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
@@ -7,3 +8,32 @@ assertEqual(head([1,2,3]),5);
 assertEqual(head(["one","two","three"]),"four");
 assertEqual(head([1]),1);
 assertEqual(head([]),2);
+*/
+
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1,2,3]",() => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']),'5');
+  });
+
+  it('should return "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns 4 for [4,5,6]", () => {
+    assert.strictEqual(head([4,5,6]),'5');
+  });
+
+  it("returns 4 for [4]", () => {
+    assert.strictEqual(head([4]),4);
+  });
+
+
+
+});
