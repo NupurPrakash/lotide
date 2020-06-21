32c266ebcd10f579438962b23b8cf17f6c0570f6
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-21 15:59:55 +0000

    Separated test files from head

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 function head(item)
 {
   return item[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1,2,3]),5);
-assertEqual(head(["one","two","three"]),"four");
-assertEqual(head([1]),1);
-assertEqual(head([]),2);
+module.exports = head;
