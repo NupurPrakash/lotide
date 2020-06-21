406097bb427b42aa7483cf037b3c3511034bcc50
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-21 21:16:40 +0000

    Incorporated mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,5 @@
+/*
+
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
@@ -8,3 +10,27 @@ assertArraysEqual(middle([1]),[]);
 assertArraysEqual(middle([1, 2]), [2]); 
 assertArraysEqual(middle([1, 2, 3]),[2]);
 
+*/
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [] for ['Hello','Lighthouse']", () => {
+    assert.deepEqual(middle(['Lighthouse','Labs']), []);
+  });
+
+  it('returns [3] for [1,2,3,4,5]', () => {
+    assert.deepEqual(middle([1,2,3,4,5]),[3]);
+  });
+
+  it('returns [2,3] for [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+
+
+});
