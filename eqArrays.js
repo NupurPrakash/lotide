94705f23a80902d909d548e2e68477e247168d56
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-21 16:35:59 +0000

    Removed test cases from eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function(array1,array2) {
   if(array1.length !== array2.length) {
     return false;
@@ -19,13 +9,4 @@ const eqArrays = function(array1,array2) {
   }
   return true;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
+module.exports = eqArrays;
