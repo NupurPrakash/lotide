91e7f50cf969da21232d42e862ffdb12bcc95cda
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-21 21:07:52 +0000

    incorporated mocha and chai';

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,10 @@
+/*
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const result = ["Hello","Lighthouse","Labs"];
 tail(result);
-assertEqual(result.length,3);
+assertEqual(result.length,3);//3 === 3
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
@@ -23,3 +24,28 @@ assertEqual(tailCheck1,1);
 const tailCheck2 = [1];
 tail(tailCheck2);
 assertEqual(tailCheck2.length,4);
+*/
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+
+  it("returns ['Lighthouse','Labs'] for ['Hello','Lighthouse','Labs']", () => {
+    assert.deepEqual(tail(['Hello','Lighthouse','Labs']), ['Lighthouse','Labs']);
+  });
+
+  it('returns [] for [1]', () => {
+    assert.deepEqual(tail([1]),[]);
+  });
+
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]),[]);
+  });
+
+
+});
